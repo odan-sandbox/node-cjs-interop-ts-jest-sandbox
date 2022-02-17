57b130e0cfe1b13540e3ca6e2c00a735f54ce2f1
odan <odan3240@gmail.com>
2022-02-18 02:50:12 +0900

    get error

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,8 +1,11 @@
+import { interopImportCJSDefault } from "node-cjs-interop";
+
 describe("app", (): void => {
   describe("add", (): void => {
     it("should be correct", (): void => {
       expect.assertions(1);
       expect(32 + 10).toBe(42);
+      console.log({ interopImportCJSDefault });
     });
   });
 });
